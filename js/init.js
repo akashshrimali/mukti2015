27793862f12f6e942b6180b9a04e55a7061eeee8
Suman Senapati <sigma.6.2009@gmail.com>
2015-01-02 15:53:41 +0530

    Revert "Beta Testing"
    This reverts commit edb2f94e4aaedf7be9a1d121c01b4ca05ce62124.

--- js/init.js
+++ js/init.js
@@ -333,12 +333,12 @@ if(is_chrome)
 			var off2=off1+ $('.carousel').outerHeight() + $('#banner').outerHeight();
 			var off3=off2+ $('.mktivid').outerHeight()+90;
 			var off4=$('#abt').offset().top + 20;
-			
-}
-	$('#nav').click(function(){
+			$('#nav').click(function(){
 				alert(off4);
 				
 			});
+}
+	
 if(is_firefox)
 {
 	var off1=$('#banner').offset().top + 20;
