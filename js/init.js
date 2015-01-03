7bf15ba314f7c6c981e43f83f148e193768a628e
Suman Senapati <sigma.6.2009@gmail.com>
2015-01-03 22:18:50 +0530

    Beta

--- js/init.js
+++ js/init.js
@@ -319,15 +319,7 @@ function onReady(callback) {
 
 $(document).ready(function(){
 	
-	$('body').jpreLoader({
-        splashID: "#jSplash",
-        showSplash: true,
-        showPercentage: true,
-        autoClose: true,
-        splashFunction: function() {
-            $('#circle').delay(250).animate({'opacity' : 1}, 500, 'linear');
-        }
-    });
+	
 	
 	
 	var width=$('.muktivid').outerWidth();
@@ -419,7 +411,15 @@ if(is_firefox)
 	});
 	
 	
-	 
+	 $('body').jpreLoader({
+        splashID: "#jSplash",
+        showSplash: true,
+        showPercentage: true,
+        autoClose: true,
+        splashFunction: function() {
+            $('#circle').delay(250).animate({'opacity' : 1}, 500, 'linear');
+        }
+    });
 	
 	
 	
