3f7f328af7ba16e4b201cde6fe74da61d5b2a6ed
Suman Senapati <sigma.6.2009@gmail.com>
2015-01-04 09:02:02 +0530

    Beta

--- js/init.js
+++ js/init.js
@@ -418,6 +418,7 @@ if(is_firefox)
         autoClose: true,
         splashFunction: function() {
             $('#circle').delay(250).animate({'opacity' : 1}, 500, 'linear');
+            $('#navlogo').attr({style:"display:block"});
         }
     });
 	
